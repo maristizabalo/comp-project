cac537f9d040f706eb13335bc4b5e8f9e5eaa4ed
maristizabalo <84209839+maristizabalo@users.noreply.github.com>
2025-06-17 10:36:24 -0500

    Permisos y restricciones en apps.modulo

--- backend/apps/modulo/views.py
+++ backend/apps/modulo/views.py
@@ -1,13 +1,27 @@
 from .models import Modulo
 from .serializers import ModuloSerializer
-from utils.transactionals import ListCreateAPIView, RetrieveUpdateAPIView  # O tu base view donde tengas las transactionals
+from utils.transactionals import ListCreateAPIView, RetrieveUpdateAPIView
+from utils.permissions import CheckPermissions 
+from utils.constants import PermisoAdminEnum
+from rest_framework import permissions
+from functools import partial
 
 class ModuloListCreateView(ListCreateAPIView):
     queryset = Modulo.objects.select_related('categoria').all()
     serializer_class = ModuloSerializer
+    
+    def get_permissions(self):
+        if self.request.method in ['POST']:
+            return [permission() for permission in (partial(CheckPermissions, [PermisoAdminEnum.ADMIN_MODULO.value]),)]
+        return [permissions.AllowAny()]
 
 
 class ModuloRetrieveUpdateView(RetrieveUpdateAPIView):
     queryset = Modulo.objects.select_related('categoria').all()
     serializer_class = ModuloSerializer
     lookup_field = 'pk'
+    
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'PATCH']:
+            return [permission() for permission in (partial(CheckPermissions, [PermisoAdminEnum.ADMIN_MODULO.value]),)]
+        return [permissions.AllowAny()]
